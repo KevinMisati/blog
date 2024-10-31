71d49b836bb0d18db4a425d6cda6c20157526d4b
KevinMisati <kevinmosigisi2001@gmail.com>
2024-10-31 07:59:10 +0300

    added permissions

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blog.models import Post, Category
+from rest_framework.test import APIClient
+from django.urls import reverse
+from rest_framework import status
 
 
 class Test_Create_Post(TestCase):
@@ -31,3 +34,29 @@ class Test_Create_Post(TestCase):
         self.assertEqual(status, 'published')
         self.assertEqual(str(post), "Post Title")
         self.assertEqual(str(cat), "django")
+
+    def test_post_update(self):
+        client = APIClient()
+        self.test_category = Category.objects.create(name='django'
+        )
+        self.testuser1 = User.objects.create_user(username="test_user2",password="12345678")
+        self.testuser2 = User.objects.create_user(username="test_user3",password="12345678")
+
+        test_post = Post.objects.create(
+            category_id=1, title='Post Title', excerpt='Post Excerpt', content='Post Content', slug='post-title', author_id=1, status='published')
+        test_post.save()
+
+        client.login(username=self.testuser1.username,password='12345678')
+
+        url = reverse(('blog_api:detailcreate'),kwargs={'pk':2})
+        response = client.put(
+            url,{
+                "title": "Title",
+                "author": 2,
+                "excerpt": "Exercept",
+                "content": "Content",
+                "status": "published"
+            }, format='json'
+        )
+        print(f'n/n/n/n/n/n/n/n/n/n{response.data}/n/n/n')
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,11 +1,24 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAdminUser,DjangoModelPermissionsOrAnonReadOnly,BasePermission,SAFE_METHODS
 from blog.models import Post
 from .serializers import PostSerializer
 
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only.'
+
+    def has_object_permission(self,request,view,obj):
+        if request.method in  SAFE_METHODS:
+            return True
+        
+        return obj.author == request.user
+
+
 class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveDestroyAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView,PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
     queryset = Post.objects.all()
     serializer_class = PostSerializer

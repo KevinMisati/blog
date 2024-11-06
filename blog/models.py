65681dda4289efed9b6048db0292f0696bfac7bd
KevinMisati <kevinmosigisi2001@gmail.com>
2024-11-06 06:49:22 +0300

    added simple jwt

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.utils import timezone
+
+User =  settings.AUTH_USER_MODEL
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
